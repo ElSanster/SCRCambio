e4198cfe8fed3971b64d545b0f429a8a007dd9f1
ElSanster <134859584+ElSanster@users.noreply.github.com>
2026-01-18 22:42:53 -0500

    BREAKING: Cambio Suave y materialTheme - refactor: Movido el codigo de switch_home_screen a main.dart, para evitarme inconvenientes. - feat: Uso de MaterialTheme, Theme y cosas relacionadas para implementar el cambio del modo oscuro al claro. - feat: Implementado el cambio de modo claro a oscuro suavemente gracias a materialTheme, cambios se aplican en la pestaña de configuración como en la pantalla principal. - feat: Uso de SharedPersistance para almacenar la configuración de (por ahora) el modo de tema de la app. - fix: La hitbox de el detector de gestiones ocupa toda la pantalla usando un Scaffolding como hijo, esto implica el uso de builders y heredación de su context.
    Por ahora tengo que ver como dejar opciones para implementar el color de tema de dispositivos con materialTheme, usar colores predeterminados seleccionados por su señoría
    sin dejar de lado material theme, o que el usuario escoga los colores a usar. También que al iniciar la app use el modo de tema del sistema.

--- lib/core/text_styles.dart
+++ lib/core/text_styles.dart
@@ -1,13 +1,23 @@
 import 'package:flutter/material.dart';
+
+///Fuentes de texto para el modo oscuro (Letra blanca que resalta con un fondo oscuro)
 class TextstylesDark {
+  ///Tamaño:18
   static const TextStyle bodyText = TextStyle(color: Colors.white, fontSize: 18);
+  ///Tamaño 40 + Negrilla
   static const TextStyle title = TextStyle(color: Colors.white, fontSize: 40, fontWeight: FontWeight.bold);
+  ///Tamaño 140 + Negrilla
   static const TextStyle homeTitle = TextStyle(color: Colors.white, fontSize: 140, fontWeight: FontWeight.bold);
 
 }
+
+///Fuentes de texto para el modo oscuro (Letra negra que resalta con un fondo claro)
 class TextstylesLight {
+  ///Tamaño:18
   static const TextStyle bodyText = TextStyle(color: Colors.black, fontSize: 18);
+  ///Tamaño 40 + Negrilla
   static const TextStyle title = TextStyle(color: Colors.black, fontSize: 40, fontWeight: FontWeight.bold);
+  ///Tamaño 140 + Negrilla
   static const TextStyle homeTitle = TextStyle(color: Colors.black, fontSize: 140, fontWeight: FontWeight.bold);
 
 }
