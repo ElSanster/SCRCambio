b1a01030847d91253030a717585af34e93f40c9b
ElSanster <134859584+ElSanster@users.noreply.github.com>
2026-02-08 15:01:12 -0500

    fix(actions): Porteado a windows para realizar builds de windows

---
+++ .github/build_release.ps1
@@ -0,0 +1,41 @@
+# Este es un script para generar los builds de los que soporta el tag actual
+
+Write-Host "Obtener paquetes (flutter pub get)"
+flutter pub get
+
+Write-Host "Decodificando keystore para firmado usando base64"
+$keystoreBase64 = "${{ secrets.ANDROID_KEYSTORE }}"
+$keystoreBytes = [Convert]::FromBase64String($keystoreBase64)
+Set-Content -Path "android/upload-keystore.jks" -Value $keystoreBytes -Encoding Byte
+
+Write-Host "Creando key.properties para firmado"
+$keyPropertiesContent = @"
+storeFile=upload-keystore.jks
+storePassword=${{ secrets.KEYSTORE_PASSWORD }}
+keyAlias=${{ secrets.KEY_ALIAS }}
+keyPassword=${{ secrets.KEY_PASSWORD }}
+"@
+Set-Content -Path "android/key.properties" -Value $keyPropertiesContent
+
+Write-Host "Testear (flutter test)"
+flutter test
+
+Write-Host "Generar apk universal (flutter build apk --flavor production)"
+flutter build apk --flavor production
+
+Write-Host "Generar apks por arquitectura (flutter build apk --split-per-abi --flavor production)"
+flutter build apk --split-per-abi --flavor production
+
+Write-Host "Generar ejecutable para windows (flutter build windows)"
+flutter build windows
+
+Write-Host "Comprimir ejecutable para windows"
+Compress-Archive -Path "build/windows/x64/runner/Release/*" -DestinationPath "build/windows/x64/runner/Release/Windows-x64.zip"
+
+Write-Host "Renombrar apks correspondientes a su arquitectura"
+Move-Item -Path "build/app/outputs/flutter-apk/app-armeabi-v7a-production-release.apk" -Destination "build/app/outputs/flutter-apk/Android-armeabi-v7a.apk"
+Move-Item -Path "build/app/outputs/flutter-apk/app-arm64-v8a-production-release.apk" -Destination "build/app/outputs/flutter-apk/Android-arm64-v8a.apk"
+Move-Item -Path "build/app/outputs/flutter-apk/app-x86_64-production-release.apk" -Destination "build/app/outputs/flutter-apk/Android-x86-64.apk"
+Move-Item -Path "build/app/outputs/flutter-apk/app-production-release.apk" -Destination "build/app/outputs/flutter-apk/Android-universal.apk"
+
+Write-Host "Finalizado generación de apks y renombrado"
