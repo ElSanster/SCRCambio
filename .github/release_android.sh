620213fc7b4c23b1c41fc12526942b86e407928e
ElSanster <134859584+ElSanster@users.noreply.github.com>
2026-02-10 13:21:36 -0500

    fix(actions): Mover los archivos apk para release

--- .github/release_android.sh
+++ .github/release_android.sh
@@ -10,6 +10,7 @@ echo storePassword=$RELEASE_STOREPASSWORD >> key.properties
 echo keyPassword=$RELEASE_KEYPASSWORD >> key.properties
 echo keyAlias=$RELEASE_KEYALIAS >> key.properties
 echo storeFile=release.jks >> key.properties
+cd -
 
 echo "----Generar apk universal (flutter build apk --flavor production)"
 flutter build apk --flavor production
@@ -18,11 +19,9 @@ echo "----Generar apks por arquitectura (flutter build apk --split-per-abi --fla
 flutter build apk --split-per-abi --flavor production
 
 echo "----Renombrar apks correspondientes a su arquitectura (con mv en build/app/outputs/flutter-apk/)"
-cd -
-cd build/app/outputs/flutter-apk/
-mv app-armeabi-v7a-production-release.apk release/Android-armeabi-v7a.apk
-mv app-arm64-v8a-production-release.apk release/Android-arm64-v8a.apk
-mv app-x86_64-production-release.apk release/Android-x86-64.apk
-mv app-production-release.apk release/Android-universal.apk
+mv build/app/outputs/flutter-apk/app-armeabi-v7a-production-release.apk release/Android-armeabi-v7a.apk
+mv build/app/outputs/flutter-apk/app-arm64-v8a-production-release.apk release/Android-arm64-v8a.apk
+mv build/app/outputs/flutter-apk/app-x86_64-production-release.apk release/Android-x86-64.apk
+mv build/app/outputs/flutter-apk/app-production-release.apk release/Android-universal.apk
 
 echo "Finalizado generación de apks y renombrado"
